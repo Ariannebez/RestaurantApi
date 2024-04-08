69228789a44679c717169924927ee69326bbeb18
Ariannebez <72392619+Ariannebez@users.noreply.github.com>
2024-04-08 13:32:28 +0200

    Updating client

---
+++ api/updateClient.php
@@ -0,0 +1,30 @@
+<?php
+//THESE ARE RTHE END POINTS 
+// Set endpoint headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+ 
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+ 
+// initialize API
+include_once('../core/initialize.php');
+ 
+// Create instance of User
+$Clients = new Clients($db);
+
+$data = json_decode(file_get_contents('php://input'));
+
+$Clients->id = $data->id;
+$Clients->email = $data->email;
+$Clients->password = $data->password;
+$Clients->name = $data->name;
+$Clients->surname = $data->surname;
+$Clients->dob = $data->dob;
+
+if($Clients->update()){
+    echo json_encode(array('message' => 'User updated.'));
+}
+else{
+    echo json_encode(array('message' => 'User not updated.'));
+}
--- core/clients.php
+++ core/clients.php
@@ -92,4 +92,39 @@ class Clients{
         return false;
     }
 
+    //Update client details
+    public function update(){
+        $query = 'UPDATE '.$this->table.'
+        SET email = :email,
+        password = :password,
+        name = :name,
+        surname = :surname,
+        dob = :dob
+        WHERE id = :id;';
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->password = htmlspecialchars(strip_tags($this->password));
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->surname = htmlspecialchars(strip_tags($this->surname));
+        $this->dob = htmlspecialchars(strip_tags($this->dob));
+
+        //bind thr parameters to request
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':email', $this->email);
+       $stmt->bindParam(':password', $this->password); // Ensure password is securely hashed
+       $stmt->bindParam(':name', $this->name);
+       $stmt->bindParam(':surname', $this->surname);
+       $stmt->bindParam(':dob', $this->dob);
+
+        if($stmt->execute()){
+            return true;
+        }
+
+        printf('Error: %s. \n', $stmt->error);
+        return false;
+    }
+
 }
