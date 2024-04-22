ee8a3345143dfe41c30b87b40885e1c932c9bd74
Ariannebez <72392619+Ariannebez@users.noreply.github.com>
2024-04-22 09:07:38 +0200

    Getting country by id

---
+++ api/getCountryById.php
@@ -0,0 +1,32 @@
+<?php
+// Set endpoint headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+// Initialize API
+include_once('../core/initialize.php');
+
+// Create instance of User
+$country = new country($db);
+
+
+// Attempt to set the client ID from the GET request, or end execution if not provided
+$country->id = isset($_GET['id']) ? $_GET['id'] : die(json_encode(['message' => 'Client ID not provided.']));
+
+$found = $country->read_single();
+
+if (!$found) {
+    // If no client was found
+    echo json_encode(['message' => 'No Country not found with this id.']);
+    exit;
+}
+
+if ($country) {
+    $country_info = array(
+        'id' => $country->id,
+        'name' => $country->name
+    );
+    echo json_encode($country_info);
+} else {
+    echo json_encode(['message' => 'Error: Access denied.']);
+}
